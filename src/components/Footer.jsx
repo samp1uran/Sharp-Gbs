20115fb0fab7fb38c9fd4daf8b6ffec5c9940d96
Harshit bhandari <hbhandari0005@gmail.com>
2025-08-05 09:17:44 +0530

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,43 @@
-function Footer() {
+// src/components/Footer.jsx
+import { FaLinkedin, FaFacebookSquare } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
   return (
-    <footer className=" w-full bg-gray-800 text-white text-center text-sm md:text-base py-4">
-      &copy; 2024 Sharp Gbs. All rights reserved.
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between space-y-4 md:space-y-0">
+        <Link to="/" className="flex items-center gap-2">
+          <img
+            src="/Logo.jpg"
+            alt="Sharp Gbs Logo"
+            className="w-7 h-7 rounded-full object-cover"
+          />
+          <div className="font-bold ">Sharp Gbs</div>
+        </Link>
+
+        <div className="flex items-center space-x-6">
+          <a
+            href="https://www.linkedin.com/company/sharp-gbs/?viewAsMember=true"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2 hover:text-blue-400 transition"
+          >
+            <FaLinkedin className="text-xl" />
+            <span>LinkedIn</span>
+          </a>
+          <a
+            href="https://www.facebook.com/people/Sharp-Gbs/100090655917480/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2 hover:text-blue-400 transition"
+          >
+            <FaFacebookSquare className="text-xl" />
+            <span>Facebook</span>
+          </a>
+        </div>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
